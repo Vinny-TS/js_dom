65b443fde1e624276724ec1e423c324f99213f83
Vinícius Silva <vivisec007@gmail.com>
2021-06-27 18:50:16 -0300

    Finalização

--- js/listadetarefas.js
+++ js/listadetarefas.js
@@ -1,51 +1,61 @@
 (() => {
     const novaTarefa = document.querySelector('[data-form-button]')
-
-    const inputTarefa = document.querySelector('[data-form-input]')
-
+    
     function criarBotaoDelete() {
         const botaoDelete = document.createElement('span')
-        botaoDelete.classList = "close"
         botaoDelete.innerText = "x"
+        botaoDelete.classList = "close"
+
         botaoDelete.addEventListener('click', deletarTarefa)
-        return botaoDelete;
+
+        return botaoDelete
+    }
+
+    function criarBotaoConcluir() {
+        const botaoConcluir = document.createElement('input')
+        botaoConcluir.setAttribute('type', 'checkbox')
+        botaoConcluir.classList = 'form-check-input'
+
+        botaoConcluir.addEventListener('click', concluirTarefa)
+
+        return botaoConcluir
     }
 
     function deletarTarefa(evento) {
-        const BotaoDeletarClicado = evento.target
-        const TarefaDeletar = BotaoDeletarClicado.parentElement
-        console.log(TarefaDeletar)
-        TarefaDeletar.remove()
-    }
-
-    function criarCheckbox() {
-        const checkBox = document.createElement('input')
-        checkBox.setAttribute("type", "checkbox");
-        checkBox.classList = "checkbox"
-        checkBox.addEventListener('click', concluirTarefa)
-        return checkBox;
+        const botaoDeleteClicado = evento.target
+        const itemDaLista = botaoDeleteClicado.parentElement
+        itemDaLista.remove()
     }
 
     function concluirTarefa(evento) {
-        const checkboxClicado = evento.target
-        const TarefaConcluida = checkboxClicado.parentElement
-        TarefaConcluida.classList.toggle('done')
+        const botaoConcluirClicado = evento.target
+        const itemDaListaConcluido = botaoConcluirClicado.parentElement
+        itemDaListaConcluido.classList.toggle('tarefa_concluida')
     }
-
 
     function criarTarefa(evento) {
         evento.preventDefault()
-        const valorTarefa = " - " + inputTarefa.value
+
+        const inputTarefa = document.querySelector('[data-form-input]')
+        const valorTarefa = inputTarefa.value
+
         const listaDeTarefas = document.querySelector('[data-task]')
+
         novaLabel = document.createElement('label')
-        novaLabel.innerText = valorTarefa
+        novaLabel.innerText = `- ${valorTarefa}`;
         novaLabel.className = "form-check-label"
+
         novoItem = document.createElement('li')
-        novoItem.appendChild(criarCheckbox())
+
+        novoItem.appendChild(criarBotaoConcluir())
         novoItem.appendChild(novaLabel)
         novoItem.appendChild(criarBotaoDelete())
+
         listaDeTarefas.appendChild(novoItem)
+
         inputTarefa.value = ""
+
     }
+
     novaTarefa.addEventListener('click', criarTarefa)
 })()
